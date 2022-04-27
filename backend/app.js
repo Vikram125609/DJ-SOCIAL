40d509ef48fe148d309928f74ea75127db7cc536
Vikram Singh <vs125609@gmail.com>
2022-04-27 20:58:27 +0530

    we are good to go

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const app = express();
+const route = require('./Routes/route');
 const PORT = process.env.PORT;
+
+app.use(route);
 app.listen(PORT,()=>{
     console.log(`Server is running at http://localhost:${PORT}`);
 })
